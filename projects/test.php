1f2b2495db295790b657dc8c36b87fa5ea194415
Administrator <123343103@qq.com>
2019-01-13 21:41:29 +0800

    injar

--- projects/test.php
+++ projects/test.php
@@ -2,7 +2,9 @@
 $order_money = 423548;
 $real_money = "4235.48";
 $real_money *= 100;
+
 $real_money = round($real_money);
+
 var_dump($order_money);
 var_dump($real_money);
 var_dump($order_money > $real_money);
