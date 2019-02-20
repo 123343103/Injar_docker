b3007058d5e98462f361154756e478818f312351
Administrator <123343103@qq.com>
2019-02-20 14:13:55 +0800

    injar

--- projects/test.php
+++ projects/test.php
@@ -1,8 +1,15 @@
 <?php
 
-$str=file_get_contents("houseSourceModel (1).csv");
-$str = iconv('GBK','utf-8', $str);
-echo $str;
+$arr=[3,1,8,4,9,2,5,7,6,0]; 
+$len=count($arr); 
+for($i=0;$i<$len;$i++){ 
+    for($j=$i+1;$j<$len;$j++){ 
+        if($arr[$i]>$arr[$j]){ 
+            list($arr[$i],$arr[$j])=[$arr[$j],$arr[$i]]; 
+        } 
+    } 
+} 
+var_dump($arr);
 
 
 ?>
